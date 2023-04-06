1b3affa42c9f38051d8d7dcc2bb327aee54126e0
HaydenWashburn <hayden.washburn1@gmail.com>
2023-04-06 18:54:51 -0500

    fixed it!

--- server.js
+++ server.js
@@ -13,40 +13,33 @@ http
       dataChunksArray.push(chunk);
     });
 
-    request.on('end', () => {
-      if (method == 'GET') {
-        console.log("In Post if")
+    request.on("end", () => {
+      if (method == "GET") {
         const body = JSON.parse(Buffer.concat(dataChunksArray).toString());
         const responseBody = { method, url, body };
+        console.log(url, method);
         if (url == "/") {
-            console.log("home route")
-            response.write("Heyy geeksforgeeks ", 'utf8', () => {
-                console.log("Writing string Data...");
-            });
-          response.write(JSON.stringify(responseBody));
+          console.log("home route");
+          response.end("Heyy geeksforgeeks");
+          //   response.write(JSON.stringify(responseBody));
           response.statusCode = 200;
+        } else if (url == "/about") {
+          response.write(
+            "<h2>This is the story all about how my life got flipped-turned upside down</h2>"
+          );
+          response.statusCode = 200;
+        } else if (url == "/echo") {
+          response.write("Blah");
+          request.statusCode = 202;
         } else {
-            console.log("error route")
+          console.error("route not found");
           response.write("<h1>Page Not Found</h1>");
           response.statusCode = 404;
-          //   response.end();
         }
-    }
-    response.end();
-    });
 
-    if (url == "/about") {
-      response.write(
-        "<h2>This is the story all about how my life got flipped-turned upside down</h2>"
-      );
-      response.statusCode = 200;
-      response.end();
-    }
-    if (url == "/echo") {
-      response.write("Blah");
-      request.statusCode = 202;
-    }
-    response.end();
+        response.end();
+      }
+    });
   })
   .listen(PORT, () => {
     console.log(`server is listening at local host ${PORT} port`);
